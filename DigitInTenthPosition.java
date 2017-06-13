df4d2aff98842f447358451e8193b1fd9d48d343
kameshgame <kameshseenivas555@gmail.com>
2017-06-13 11:58:22 +0530

    Create DigitInTenthPosition.java

---
+++ DigitInTenthPosition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    int s;
+    s=(N/10)%10;
+    N=s;
+    System.out.println(N);
+	}
+}
+
+
+
