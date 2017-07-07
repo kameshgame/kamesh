8f4057f14dffeac3c5f6a80819785dc6b214c57e
kameshgame <kameshseenivas555@gmail.com>
2017-07-07 06:19:03 +0530

    Create EmpolyeeNameAgeSalaryAsterick.java

---
+++ EmpolyeeNameAgeSalaryAsterick.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args)
+    {
+        String s1,s2,s3;
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+        s2=sc.nextLine();
+        s3=sc.nextLine();
+        System.out.print(s1+"\n"+"**"+"\n"+s2+"\n"+"**"+"\n"+s3);
+	}
+}
