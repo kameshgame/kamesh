e888bca9352a5ecff2502e8b152735549c3da536
kameshgame <kameshseenivas555@gmail.com>
2017-07-07 06:19:54 +0530

    Create TopRightCoordinates.java

---
+++ TopRightCoordinates.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+        int blx,bly,l,trx,try1;
+        Scanner sc=new Scanner(System.in);
+        blx=sc.nextInt();
+        bly=sc.nextInt();
+        l=sc.nextInt();
+        trx=blx+l;
+        try1=bly+l;
+        System.out.print(trx+"\n"+try1);
+	}
+}
