6a53880f2b050caa5693185d24edb18ca2e663aa
kameshgame <kameshseenivas555@gmail.com>
2017-07-07 06:31:35 +0530

    Create HypenSeperatedCoPrimes.java

---
+++ HypenSeperatedCoPrimes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) 
+    {
+		//Your Code Here
+        int n1,n2;
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        System.out.print(n1+"-"+n2);
+	}
+}
