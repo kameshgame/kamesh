2279656b802b62091f57f03991be622e4130b12b
kameshgame <kameshseenivas555@gmail.com>
2017-07-07 06:04:15 +0530

    Create Athelete&MedalCount.java

---
+++ Athelete&MedalCount.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) 
+    {
+		//Your Code Here
+        String s;
+        int n;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        n=sc.nextInt();
+        System.out.print(s+" won "+n+" medals");
+	}
+}
