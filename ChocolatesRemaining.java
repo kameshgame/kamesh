46fc8f8dfcca9872c4d58e90885492e3908a7ca7
kameshgame <kameshseenivas555@gmail.com>
2017-07-07 06:09:00 +0530

    Create ChocolatesRemaining.java

---
+++ ChocolatesRemaining.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+        int c,n,r;
+        Scanner sc=new Scanner(System.in);
+        c=sc.nextInt();
+        n=sc.nextInt();
+        r=c%n;
+        System.out.print(r);
+	}
+}
