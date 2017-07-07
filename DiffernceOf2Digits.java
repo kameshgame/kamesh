c859d4c9e9bb4b458b50b8a336fa157b6bd87b45
kameshgame <kameshseenivas555@gmail.com>
2017-07-07 06:15:46 +0530

    Create DiffernceOf2Digits.java

---
+++ DiffernceOf2Digits.java
@@ -0,0 +1,18 @@
+package maximumnumber;
+
+import java.util.Scanner;
+
+public class dif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d;
+		Scanner sc=new Scanner(System.in);
+		d=sc.nextInt();
+		d=d/10;
+		int dif;
+		dif=d+1;
+		System.out.println(dif);
+	}
+
+}
