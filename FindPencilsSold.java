7cd507f459f67a805a22806a1038c8f36a5764d0
kameshgame <kameshseenivas555@gmail.com>
2017-07-07 06:23:13 +0530

    Create FindPencilsSold.java

---
+++ FindPencilsSold.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+        int n,e,s;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        e=sc.nextInt();
+        s=n-e;
+        System.out.print(s);
+	}
+}
