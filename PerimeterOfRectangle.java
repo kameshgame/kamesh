7d3b97f877ffd78d541416952255c1e302cfce13
kameshgame <kameshseenivas555@gmail.com>
2017-07-09 14:18:37 +0530

    Create PerimeterOfRectangle.java

---
+++ PerimeterOfRectangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) 
+    {
+		//Your Code Here
+        int l,b,p;
+        Scanner sc=new Scanner(System.in);
+        l=sc.nextInt();
+        b=sc.nextInt();
+        p=2*(l+b);
+        System.out.print(p);
+	}
+}
