90a83512c8bf1558320b95b156d771d6a2353f79
kameshgame <kameshseenivas555@gmail.com>
2017-07-09 14:25:14 +0530

    Create PrintFromMToN.java

---
+++ PrintFromMToN.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+    Scanner sc=new Scanner(System.in);
+    int M=sc.nextInt();
+    int N=sc.nextInt();
+    int I;
+    for(I=M;I<=N;I++){
+        System.out.print(I+" ");
+    }
+	}
+}
