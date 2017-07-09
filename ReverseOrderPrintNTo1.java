0a50f0a4fbcd5717c4a037bb109bc87ee574504e
kameshgame <kameshseenivas555@gmail.com>
2017-07-09 13:53:15 +0530

    Create ReverseOrderPrintNTo1.java

---
+++ ReverseOrderPrintNTo1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    int I;
+    for(I=N;I>0;I--){
+        System.out.print(I+" ");
+    }
+	}
+}
