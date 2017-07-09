582df36bd654c0acdfbabe7b3bdf311cb9e3cdb7
kameshgame <kameshseenivas555@gmail.com>
2017-07-09 13:50:35 +0530

    Create RepeatTheInputNumber.java

---
+++ RepeatTheInputNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        System.out.print("You entered "+n);
+	}
+}
