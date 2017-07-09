2fdb3150de69c3bfddd5eee9815a1a293b48c2d1
kameshgame <kameshseenivas555@gmail.com>
2017-07-09 14:19:31 +0530

    Create PerimeterSquareArea.java

---
+++ PerimeterSquareArea.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+    int p,a;
+    Scanner sc=new Scanner(System.in);
+    p=sc.nextInt();
+    a=(p*p)/16;
+    System.out.print(a);
+	}
+}
