b0f493d111783745f19f4b8ea0c29c976bcf0d06
kameshgame <kameshseenivas555@gmail.com>
2017-07-09 13:49:47 +0530

    Create RailwayTimeDisplay.java

---
+++ RailwayTimeDisplay.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) 
+    {
+		//Your Code Here
+        int h,m,s;
+        Scanner sc=new Scanner(System.in);
+        h=sc.nextInt();
+        m=sc.nextInt();
+        s=sc.nextInt();
+        System.out.print(h+":"+m+":"+s);
+	}
+}
