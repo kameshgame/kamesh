65fdaf98a9adcc4a3a00d5d077fb0861f8421be4
kameshgame <kameshseenivas555@gmail.com>
2017-07-09 14:06:29 +0530

    Create SumOfAgesOfHusbandAndWife.java

---
+++ SumOfAgesOfHusbandAndWife.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+        int h,w,sum;
+        Scanner sc=new Scanner(System.in);
+        h=sc.nextInt();
+        w=sc.nextInt();
+        sum=h+w;
+        System.out.print(sum);
+	}
+}
