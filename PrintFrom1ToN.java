c6a42d10587f78afe6e5cf42936c4ec5a1077c3f
kameshgame <kameshseenivas555@gmail.com>
2017-07-09 14:24:38 +0530

    Create PrintFrom1ToN.java

---
+++ PrintFrom1ToN.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    int I;
+    for(I=1;I<=N;I++){
+        System.out.print(I+" ");
+    }
+	}
+}
